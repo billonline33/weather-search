e6a4c5767d6ce901d27b195194fe1a78f614beaa
billonline33 <billonline33@hotmail.com>
2018-07-23 00:32:46 +1000

    feature(action): handle location page back button click

--- src/components/Layout/location.js
+++ src/components/Layout/location.js
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { history } from "../../store";
 import { Button } from "react-bootstrap";
+import { withRouter } from "react-router";
 
-const Location1 = () => {
-  return (
-    <div>
-      <h2>Today</h2>
-      <div>
-        <span>Max</span>
-        <span>24C</span>
-      </div>
-      <div>
-        <span>Min</span>
-        <span>16C</span>
-      </div>
-      <div>
-        <span>Humidity</span>
-        <span>56%</span>
-      </div>
-      <div>
-        <span>Visibility</span>
-        <span>10.3 miles</span>
-      </div>
-      <div>
-        <span>Pressure</span>
-        <span>1018mb</span>
-      </div>
-      <div>
-        <span>Confidence</span>
-        <span>73%</span>
-      </div>
-      <div>
-        <Button bsStyle="primary" bsSize="large" onClick={history.push("/")}>
-          Back
-        </Button>
-      </div>
-    </div>
-  );
-};
+class Location1 extends Component {
+  constructor(props) {
+    super(props);
 
-export default Location1;
+    this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
+  }
+
+  handleBackButtonClick() {
+    this.props.history.push("/");
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Today</h2>
+        <div>
+          <span>Max</span>
+          <span>24C</span>
+        </div>
+        <div>
+          <span>Min</span>
+          <span>16C</span>
+        </div>
+        <div>
+          <span>Humidity</span>
+          <span>56%</span>
+        </div>
+        <div>
+          <span>Visibility</span>
+          <span>10.3 miles</span>
+        </div>
+        <div>
+          <span>Pressure</span>
+          <span>1018mb</span>
+        </div>
+        <div>
+          <span>Confidence</span>
+          <span>73%</span>
+        </div>
+        <div>
+          <Button
+            bsStyle="primary"
+            bsSize="large"
+            onClick={this.handleBackButtonClick}
+          >
+            Back
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Location1);
